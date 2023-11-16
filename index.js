9c54e51d3b86c1cdd61bbae81f982b47ccbad9f6
Miranda Lucas <mirandalucas@Mirandas-MacBook-Pro.local>
2023-11-16 14:06:41 -0600

    workshop edits

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-    res.sendFile(__dirname + `/public/index.html`)
+    res.sendFile(__dirname + '/public/index.html')
 });
 
 // hello world route
@@ -21,30 +21,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-    res.status(200).send(pets)
+    res.send(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-    
+    const { owner } = req.query
     
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.filter(
+        (pet) => pet.owner.toLowerCase() === owner.toLowerCase());
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-    const params = req.params
+    const name = req.params.name
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === params);
+    const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-    res.status(200).send(pet)
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
